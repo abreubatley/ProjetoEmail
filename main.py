1c74c255c3ada2b8b887228586e083b72a0a78be
abreubatley <batley.fabreu@gmail.com>
2022-01-09 13:15:17 -0300

    Update main.py
    Updated main to adapt project

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello World!")
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def email():
+    message = MIMEMultipart()
+
+    emailSend = input('Informe seu email (aceitamos apenas gmail e hotmail): ')
+    message['From'] = emailSend
+
+    pinword = input('Informe sua senha: ')
+    password = pinword
+
+    emailReceive = input('Informe o email destinatário: ')
+    message['To'] = emailReceive
+
+    message['Subject'] = "Email automático enviado com sucesso!"
+
+    body = "Olá! Você recebeu um email de teste do Batley"
+    message.attach(MIMEText(body, 'plain'))
+
+    if "gmail.com" not in emailSend:
+        smtpSend = 'smtp.live.com'
+    else: 
+        smtpSend = 'smtp.gmail.com'
+
+    smtp = smtplib.SMTP(smtpSend, port=587)
+    smtp.starttls()
+    smtp.login(message['From'], password)
+    smtp.sendmail(message['From'], message['To'], message.as_string())
+    smtp.quit()
+
+if __name__ == "__main__":
+    email()
+    print("Enviado com sucesso!")
